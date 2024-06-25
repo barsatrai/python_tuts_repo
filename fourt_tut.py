1f8080541542db459db0ba45958d05f12cc02c41
barsatrai <barsatrai777@gmail.com>
2024-06-25 16:35:50 +0545

    operators

---
+++ fourt_tut.py
@@ -0,0 +1,48 @@
+# operators in python
+
+print(5+5) #addition
+print(5-5) #subtraction
+print(5*5) #multiply
+print(5/5) #divide
+print(5//5) #quetient
+print(5**5) #exponential
+print(5%5) #remainder
+
+# #calculator in python
+#==============
+x = float(input("entr x:"))
+y = float(input("entr y:"))
+print("addition is x+y=",x+y)
+print("subtraction is x-y=",x-y)
+print("multiplication is x*y=",x*y)
+print("divisiion is x/y=",x/y)
+
+#using else-if statement
+# a = float(input("Enter first number: "))
+# b = float(input("Enter second number: "))
+# print("Enter operation:\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division")
+# value = int(input("Enter the number corresponding to the operation: "))
+
+# # Define the switch_case function
+# def switch_case(value, a, b):
+#     if value == 1:
+#         return a + b
+#     elif value == 2:
+#         return a - b
+#     elif value == 3:
+#         return a * b
+#     elif value == 4:
+#         if b != 0:
+#             return a / b
+#         else:
+#             return "Error! Division by zero."
+#     else:
+#         return "Undefined operation!!"
+
+# result = switch_case(value, a, b)
+# print("Result:", result)
+
+
+
+ 
+
