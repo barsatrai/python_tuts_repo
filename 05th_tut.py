9a9862cb8d98f41dfb29c24c8ece1a76cc5534f3
barsatrai <barsatrai777@gmail.com>
2024-06-26 18:05:54 +0545

    Type Casting

---
+++ 05th_tut.py
@@ -0,0 +1,12 @@
+# type casting in python
+#explict type casting
+#-----------------
+a = "2"
+b = "4"
+print(int(a)+int(b))
+
+#explicit type casting
+x =2.3
+y = 5
+print(x+y)
+print(type(x),type(y))
