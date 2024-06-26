1295a67f699c78b819d776f7fb45e287691164a4
barsatrai <barsatrai777@gmail.com>
2024-06-26 18:03:55 +0545

    Variables

---
+++ 03rd_tut.py
@@ -0,0 +1,19 @@
+# Variables in pythons
+#--------------
+
+a = 7
+print(a)
+b = "Barsat"
+print(b)
+print(type(a))
+c = True
+print(type(c))
+d = complex(2,3)
+print(d)
+print("type of d is:", type(d))
+list = [1,2,3,['apple','banana']]
+print(list)
+data1 = {"name=":"barsat", "age=": 21, "adress=": "taplejung"}
+data2 = {"name=":"barsa", "age=": 61, "adress=": "taple"}
+data3 = {"name=":"barist", "age=": 29, "adress=": "jung"}
+print(data1,data2, data3)
