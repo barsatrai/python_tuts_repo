dba73e2a7049c57d1757d73cc0d4c77b3ce4b8e1
barsatrai <barsatrai777@gmail.com>
2024-06-26 18:06:50 +0545

    User input

---
+++ 06th_tut.py
@@ -0,0 +1,9 @@
+# User input in python
+
+a = str(input("Enter first name:"))
+b = str(input("Enter your surname:"))
+print("your name is:",str(a)+str(b))
+
+x = input("enter a number:")
+y = input("enter second number:")
+print(int(x)+int(y))
